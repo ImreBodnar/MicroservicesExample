f0549f0734d917e9055c4ebf9ccd33f0b9958b54
Imre Bodnár <bodnar.imre.85@gmail.com>
2025-04-04 00:32:01 +0200

    From Section1 to Section5

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface ICommand<out Tresponse> : IRequest<Tresponse>
+    {
+    }
+
+    public interface ICommand : ICommand<Unit>
+    {
+    }
+}
