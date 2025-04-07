b694ba92304d66a21b89a2230bcfb4f8a5587859
Imre Bodnár <bodnar.imre.85@gmail.com>
2025-04-07 20:23:44 +0200

    Adding specific exception classes

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -3,15 +3,15 @@
     public class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
         : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
     {
-        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
         {
-            logger.LogInformation($"GetProductByIdQueryHandler.Handle called with {request}");
+            logger.LogInformation($"GetProductByIdQueryHandler.Handle called with {query}");
 
-            var product = await session.LoadAsync<Product>(request.Id, cancellationToken);
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
 
             if (product is null)
             {
-                throw new ProductNotFoundException();
+                throw new ProductNotFoundException(query.Id);
             }
 
             return new GetProductByIdResult(product);
--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -13,7 +13,7 @@ namespace Catalog.API.Products.UpdateProduct
 
             if (product is null)
             {
-                throw new ProductNotFoundException();
+                throw new ProductNotFoundException(command.Id);
             }
 
             product.Name = command.Name;
