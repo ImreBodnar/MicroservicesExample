b7c1ac4859e1ddc48a62588e5fe8b00c12802b77
ibodnar <ibodnar@cleware.hu>
2025-04-08 15:57:07 +0200

    Add data seeding

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogInitialData.cs
@@ -0,0 +1,34 @@
+﻿using Marten.Schema;
+
+namespace Catalog.API.Data
+{
+    public class CatalogInitialData : IInitialData
+    {
+        public async Task Populate(IDocumentStore store, CancellationToken cancellation)
+        {
+            using var session = store.LightweightSession();
+
+            if (await session.Query<Product>().AnyAsync())
+                return;
+
+            session.Store<Product>(GetPreconfiguredProducts());
+            await session.SaveChangesAsync();
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Id = new Guid(),
+                    Name = "IPhone X",
+                    Description = "This is a cool phone!",
+                    ImageFile = "product1.png",
+                    Price = 1120,
+                    Category = new List<string>{ "mobile", "apple" }
+                }
+            };
+        }
+    }
+}
