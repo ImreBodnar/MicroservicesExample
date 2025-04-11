e20d170465787d3ca866a3b7cf113ea7e8c950ca
Imre Bodnár <bodnar.imre.85@gmail.com>
2025-04-11 19:32:16 +0200

    Basket.API: implementing DeleteBasket command

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCarter();
+
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
 var app = builder.Build();
+
+app.MapCarter();
 
 app.Run();
