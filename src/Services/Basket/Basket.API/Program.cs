77d9c9c51dfe6fcd9884b9fe3903f9d656be2444
Imre Bodnár <bodnar.imre.85@gmail.com>
2025-04-18 21:22:43 +0200

    Basket.API: bypassing SSL problem

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -38,7 +38,16 @@ builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
     options =>
     {
         options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
-    });
+    })
+    .ConfigurePrimaryHttpMessageHandler(() =>
+        {
+            var handler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+            };
+
+            return handler;
+        });
 
 //Cross-cutting services
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
