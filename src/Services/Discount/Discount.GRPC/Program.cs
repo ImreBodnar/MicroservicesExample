acecf781fdcf6aa766aaf7b1d8691e770c49e4da
Imre Bodnár <bodnar.imre.85@gmail.com>
2025-04-18 18:54:30 +0200

    Discount.GRPC: removing unnecessary rows

--- src/Services/Discount/Discount.GRPC/Program.cs
+++ src/Services/Discount/Discount.GRPC/Program.cs
@@ -1,11 +1,9 @@
-using Discount.Grpc;
 using Discount.GRPC.Data;
 using Discount.GRPC.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddGrpc();
 builder.Services.AddDbContext<DiscountContext>(options =>
 {
@@ -14,7 +12,6 @@ builder.Services.AddDbContext<DiscountContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseMigration();
 app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
