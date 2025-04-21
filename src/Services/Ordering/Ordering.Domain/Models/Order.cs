fde412804580ecc53a64638bca27cf3abf5e0197
Imre Bodnár <bodnar.imre.85@gmail.com>
2025-04-21 23:56:57 +0200

    Ordering.Domain: adding functionality to models

--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -23,5 +23,61 @@
             get => OrderItems.Sum(x => x.Price * x.Quantity);
             private set { }
         }
+
+        public static Order Create(
+            OrderId id,
+            CustomerId customerId,
+            OrderName orderName,
+            Address shippingAddress,
+            Address billingAddress,
+            Payment payment)
+        {
+            var newOrder = new Order
+            {
+                Id = id,
+                CustomerId = customerId,
+                OrderName = orderName,
+                ShippingAddress = shippingAddress,
+                BillingAddress = billingAddress,
+                Payment = payment,
+                Status = OrderStatus.Pending
+            };
+
+            newOrder.AddDomainEvent(new OrderCreatedEvent(newOrder));
+
+            return newOrder;
+        }
+
+        public void Update(
+            OrderName orderName,
+            Address shippingAddress,
+            Address billingAddress,
+            Payment payment,
+            OrderStatus status)
+        {
+            OrderName = orderName;
+            ShippingAddress = shippingAddress;
+            BillingAddress = billingAddress;
+            Payment = payment;
+            Status = status;
+
+            AddDomainEvent(new OrderUpdatedEvent(this));
+        }
+
+        public void Add(ProductId productId, int quantity, decimal price)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+            var newOrderItem = new OrderItem(Id, productId, quantity, price);
+            _orderItems.Add(newOrderItem);
+        }
+
+        public void Remove(ProductId productId)
+        {
+            var orderItem = _orderItems.FirstOrDefault(x => x.ProductId == productId);
+            if (orderItem is not null)
+                _orderItems.Remove(orderItem);
+        }
     }
 }
