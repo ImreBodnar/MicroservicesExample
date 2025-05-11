27721d8da30b30df8f728654c3047db139da3167
Imre Bodnár <bodnar.imre.85@gmail.com>
2025-05-11 23:05:24 +0200

    Ordering.Application: adding BasketCheckoutEventHandler

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Integration/BasketCheckoutEventHandler.cs
@@ -0,0 +1,53 @@
+﻿using BuildingBlocks.Messaging.Events;
+using MassTransit;
+using Ordering.Application.Orders.Commands.CreateOrder;
+
+namespace Ordering.Application.Orders.EventHandlers.Integration
+{
+    public class BasketCheckoutEventHandler
+        (ISender sender, ILogger<BasketCheckoutEventHandler> logger)
+        : IConsumer<BasketCheckOutEvent>
+    {
+        public async Task Consume(ConsumeContext<BasketCheckOutEvent> context)
+        {
+            logger.LogInformation("Integration Event handled: {IntegrationEvent}", context.Message.GetType().Name);
+
+            var command = MapToCreateOrderCommand(context.Message);
+            await sender.Send(command);
+        }
+
+        private CreateOrderCommand MapToCreateOrderCommand(BasketCheckOutEvent message)
+        {
+            var addressDTO = new AddressDto(
+                message.FirstName,
+                message.LastName,
+                message.EmailAddress,
+                message.AddressLine,
+                message.Country,
+                message.State,
+                message.ZipCode);
+            var paymentDTO = new PaymentDto(
+                message.CardName,
+                message.CardNumber,
+                message.Expiration,
+                message.CVV,
+                message.PaymentMethod);
+            var orderId = Guid.NewGuid();
+
+            var orderDTO = new OrderDto(
+                orderId,
+                message.CustomerId,
+                message.UserName,
+                addressDTO,
+                addressDTO,
+                paymentDTO,
+                Ordering.Domain.Enums.OrderStatus.Pending,
+                [
+                    new OrderItemDto(orderId, new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff61"), 2, 500),
+                    new OrderItemDto(orderId, new Guid("c67d6323-e8b1-4bdf-9a75-b0d0d2e7e914"), 1, 400)
+                ]);
+
+            return new CreateOrderCommand(orderDTO);
+        }
+    }
+}
