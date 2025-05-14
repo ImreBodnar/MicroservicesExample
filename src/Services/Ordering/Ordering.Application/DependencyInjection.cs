7912f85c0e18122603f492064f44ae2e26cbccdc
Imre Bodnár <bodnar.imre.85@gmail.com>
2025-05-14 22:58:16 +0200

    Ordering.Application: adding feature manager option to the publishing of OrderCreated integration event

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -2,13 +2,15 @@
 using BuildingBlocks.Messaging.MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FeatureManagement;
 using System.Reflection;
 
 namespace Ordering.Application
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddApplicationServices
+            (this IServiceCollection services, IConfiguration configuration)
         {
             services.AddMediatR(config =>
             {
@@ -17,6 +19,7 @@ namespace Ordering.Application
                 config.AddOpenBehavior(typeof(LoggingBehavior<,>));
             });
 
+            services.AddFeatureManagement();
             services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
 
             return services;
--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,18 +1,22 @@
 ﻿using MassTransit;
+using Microsoft.FeatureManagement;
 
 namespace Ordering.Application.Orders.EventHandlers.Domain
 {
     public class OrderCreatedEventHandler
-        (IPublishEndpoint publishEndpoint, ILogger<OrderCreatedEventHandler> logger)
+        (IPublishEndpoint publishEndpoint, IFeatureManager featureManager, ILogger<OrderCreatedEventHandler> logger)
         : INotificationHandler<OrderCreatedEvent>
     {
         public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
         {
             logger.LogInformation($"Domain Event handled: {domainEvent.GetType().Name}");
 
-            var integrationEvent = domainEvent.Order.ToOrderDto();
+            if (await featureManager.IsEnabledAsync("OrderFullfiment"))
+            {
+                var integrationEvent = domainEvent.Order.ToOrderDto();
 
-            await publishEndpoint.Publish(integrationEvent, cancellationToken);
+                await publishEndpoint.Publish(integrationEvent, cancellationToken);
+            }
         }
     }
 }
